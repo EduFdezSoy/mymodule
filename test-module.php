9c155393b0ef469bc505152a566d8efd8ebaf4a2
Eduardo Fernandez <yo@edufdezsoy.es>
2017-04-23 18:46:01 +0200

    content added

--- test-module.php
+++ test-module.php
@@ -6,4 +6,26 @@ if (!defined('_PS_VERSION_'))
 
 class MyModule extends Module
 {
+    public function __construct()
+    {
+        $this->name = 'test-module';
+        $thhis->tab = 'front_office_features';
+        $this->version = '0.0.1';
+        $this->author = 'Eduardo Fernandez';
+        $this->need_isntance = 0;
+        $this->ps_versions_compilacy = array('min' => '1.7', 'max' => _PS_VERSION_);
+        $this->bootstrap = true;
+        
+        parent::construct();
+        
+        $this->displayName = $this->l('Test Module');
+        $this->description = $this->l('Descripcion tope chula del modulo.');
+        
+        $this->confirmUninstall = $this->l('Estás seguro?');
+        
+        if (!Configuration::get('MYMODULE_NAME'))
+            $this->warning = $this->l('No name provided');
+    }
+    
+    
 }
