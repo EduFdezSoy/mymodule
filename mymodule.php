c36a991a96be8e860211d89ba53f44f58b7ca4f2
Eduardo Fernandez <yo@edufdezsoy.es>
2017-04-24 13:01:46 +0200

    v0.0.2

--- mymodule.php
+++ mymodule.php
@@ -10,7 +10,7 @@ class MyModule extends Module
   {
     $this->name = 'mymodule';
     $this->tab = 'front_office_features';
-    $this->version = '0.0.1';
+    $this->version = '0.0.2';
     $this->author = 'Eduardo Fernandez';
     $this->need_instance = 0;
     $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
